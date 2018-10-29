b43d99d649c82936996c2078875cfbda616a2e24
john12458 <john210628@gmail.com>
2018-10-29 11:11:08 +0800

    Test2

--- app.py
+++ app.py
@@ -38,12 +38,13 @@ import random
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-        db=["吃大","我00","00飯"]
+        db=["吃大便","我家","炒飯""]
         dinner = random.choice(db)
         messages=[]
         messages.append(TextSendMessage(text="哈"*10))
-        messages.append(TextSendMessage(text=dinner))
-    
+      
+        if(event.message.text=="吃什麼?"):
+            messages.append(TextSendMessage(text=dinner))
         line_bot_api.reply_message(
             event.reply_token,
             messages
